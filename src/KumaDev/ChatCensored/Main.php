9b1e40d8591d1214dbfa8401773a7804ceb8e992
FunkaRevon <ardrafunka@gmail.com>
2024-11-16 23:57:32 +0700

    Add files via upload

--- src/KumaDev/ChatCensored/Main.php
+++ src/KumaDev/ChatCensored/Main.php
@@ -5,34 +5,30 @@ namespace KumaDev\ChatCensored;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\player\Player;
+use pocketmine\Server;
 
 class Main extends PluginBase implements Listener {
 
     /** @var Config */
-    private $wordConfig;
+    private $config;
 
     public function onEnable(): void {
-        $this->saveResource("word.yml");
-        $this->wordConfig = new Config($this->getDataFolder() . "word.yml", Config::YAML);
+        $this->saveResource("config.yml");
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
 
-        $this->getServer()->getPluginManager()->registerEvents(new ChatListener($this), $this);
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
-    /**
-     * Get the list of censored words.
-     *
-     * @return array
-     */
     public function getCensoredWords(): array {
-        return $this->wordConfig->get("words", []);
+        return $this->config->get("words", []);
     }
 
-    /**
-     * Censor a message if it contains censored words.
-     *
-     * @param string $message
-     * @return string
-     */
+    public function isOpAllowed(Player $player): bool {
+        return $this->config->get("allow-op", false) && Server::getInstance()->isOp($player->getName());
+    }
+
     public function censorMessage(string $message): string {
         $words = $this->getCensoredWords();
         foreach ($words as $word) {
@@ -43,5 +39,16 @@ class Main extends PluginBase implements Listener {
             }, $message);
         }
         return $message;
+    }
+
+    public function onPlayerChat(PlayerChatEvent $event): void {
+        $player = $event->getPlayer();
+        if ($this->isOpAllowed($player)) {
+            return;
+        }
+
+        $message = $event->getMessage();
+        $censoredMessage = $this->censorMessage($message);
+        $event->setMessage($censoredMessage);
     }
 }
